876e244ead6c1ce4890d3bd52db207c45418e9d1
Toggi <thorgilshjalmarsson@gmail.com>
2021-11-29 13:58:00 +0000

    Search bar initialized

--- src/views/Contacts/index.jsx
+++ src/views/Contacts/index.jsx
@@ -2,28 +2,37 @@ import React, { useState } from 'react';
 import { View } from 'react-native';
 import ContactList from '../../components/contactList';
 import data from '../../data/data.json';
+import Toolbar from '../../components/toolbar';
 
 const Contacts = function( {navigation: { navigate }} ) {
+
 	const [ contacts, setContacts ] = useState(data.contacts);
 
 	const [ selectedContacts, setSelectedContacts ] = useState([]);
 
 	const onContactLongPress = (id) => {
+		console.log({ id });
     if (selectedContacts.indexOf(id) !== -1) {
       setSelectedContacts(selectedContacts.filter((contact) => contact !== id));
-    } else {
+    }
+		else {
       setSelectedContacts([...selectedContacts, id]);
     }
   };
 
 	return(
 		<View style={{flex:1}}>
-			<ContactList 
-				onLongPress={(id) => onContactLongPress(id)}
-				selectedContacts={selectedContacts}
-				contacts={contacts}
-			/>
-		</View>)
+			<Toolbar />
+			<View style={{flex:1}}>
+				<ContactList 
+					onLongPress={(id) => onContactLongPress(id)}
+					selectedContacts={selectedContacts}
+					contacts={contacts}
+				/>
+			</View>
+		</View>
+	)
+
 };
 
 export default Contacts;
