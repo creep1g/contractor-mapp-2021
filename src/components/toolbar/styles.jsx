5322bd0c685a8abd11009491870f26456dfa6f4b
Toggi <thorgilshjalmarsson@gmail.com>
2021-11-30 09:55:22 +0000

    Search bar fixed, some stylings in header

--- src/components/toolbar/styles.jsx
+++ src/components/toolbar/styles.jsx
@@ -3,11 +3,18 @@ import { StyleSheet } from 'react-native';
 export default StyleSheet.create ({
 	
   toolbar: {
-
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'center',
     height: 80,
-    backgroundColor: '#444444',
+    backgroundColor: '#393E42',
+  },
+  toolbarAction: {
+    alignItems: 'center',
+  },
+  toolbarActionText: {
+    fontWeight: 'bold',
+    color: 'white',
+    fontSize: 16,
   },
 });
