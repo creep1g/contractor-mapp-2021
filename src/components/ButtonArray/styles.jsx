8dded60ab8ae6fc4a87d9b2e017952fe5958a41c
Toggi <thorgilshjalmarsson@gmail.com>
2021-12-01 13:25:04 +0000

    Minimal fix in details

--- src/components/ButtonArray/styles.jsx
+++ src/components/ButtonArray/styles.jsx
@@ -2,24 +2,24 @@ import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
 	buttons: {
-		marginLeft: 100,
 		flexDirection: 'row',
 		alignItems: 'center',
+		justifyContent: 'center',
 	},
 
 	icon: {
 		fontSize: 16,
 		padding: 10,
 		color: 'white',
-		elevation: 4
+		elevation: 4,
+		borderRadius: 10,
+
 	},
 
 	button:	{
-		marginLeft: 5,
-		marginRight: 5,
-		borderWidth: 2,
-		borderColor: 'black',
-		borderRadius: 4,
+		marginLeft: 10,
+		marginRight: 10,
+		borderRadius: 10,
 		elevation: 4,
 	},
 }
